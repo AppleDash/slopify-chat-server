8061091727ff9e8c65ddeb107fb4ba74b3d33a6b
AppleDash <pet@feather.horse>
2024-12-30 01:39:24 -0500

    feat: add multi-room support

---
+++ README.md
@@ -0,0 +1,8 @@
+slopify-chat-server
+===================
+
+Simple websocket server to relay and limit chat messages for Slopify.
+
+## Usage
+First time, or after an update: `pnpm install`.
+Run the server: `pnpm start`.
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+import ip from 'ip';
 import { createServer } from 'http';
 import { WebSocketServer } from 'ws';
-import { Address6 } from 'ip-address';
 import { RateLimiter } from './rate_limit.js';
 
 /* The HTTP server and crap */
@@ -9,14 +9,20 @@ const wss = new WebSocketServer({ server });
 
 /* State variables */
 const connectedUsers = new Set();
+const roomUsers = {}; // Usernames in each room
+const roomCons = {}; // WS connections in each room
 const rateLimiter = new RateLimiter(10, 60 * 1000, 5);
 
 function getMaskedRemoteAddress(addr) {
+  console.log('Addr', addr);
   // v6 address, stick a /64 mask on
-  if (addr.indexOf(':') !== -1) {
-    const v6 = new Address6(addr);
+  if (ip.isV6Format(addr)) {
+    if (ip.isPrivate(addr)) {
+      return addr;
+    }
 
-    return v6.mask(64);
+    // /64, this library is a little silly so I have to spell it out.
+    return ip.mask(addr, 'FFFF:FFFF:FFFF:FFFF:0000:0000:0000:0000');
   }
 
   return addr;
@@ -26,8 +32,24 @@ function broadcast(message) {
   wss.clients.forEach(client => client.send(JSON.stringify(message)));
 }
 
+function broadcastToRoom(room, message) {
+  for (const conn of roomCons[room]) {
+    conn.send(JSON.stringify(message));
+  }
+}
+
+function arrayDel(ary, itm) {
+  const idx = ary.indexOf(itm);
+
+  if (idx !== -1) {
+    ary.splice(idx, 1);
+  }
+}
+
 wss.on('connection', (conn, req) => {
   let addr;
+
+  // Are we behind Cloudflare?
   if ('cf-connecting-ip' in req.headers) {
     addr = getMaskedRemoteAddress(req.headers['cf-connecting-ip']);
   } else {
@@ -37,6 +59,7 @@ wss.on('connection', (conn, req) => {
   console.log('Accepting connection from', addr);
 
   let username = null;
+  let currentRoom = null;
 
   conn.on('message', (msg) => {
     msg = JSON.parse(msg.toString());
@@ -47,6 +70,7 @@ wss.on('connection', (conn, req) => {
 
       nick = nick.trim();
 
+      // Nick is too short or too long.
       if (nick.length < 1 || nick.length > 16) {
         conn.send(JSON.stringify(
           { type: 'error', error: 'nick_bounds' }
@@ -54,6 +78,7 @@ wss.on('connection', (conn, req) => {
         return;
       }
 
+      // Nick already taken
       if (connectedUsers.has(nick)) {
         conn.send(JSON.stringify(
           { type: 'error', error: 'nick_taken' }
@@ -61,6 +86,7 @@ wss.on('connection', (conn, req) => {
         return;
       }
 
+      // If they're changing their name from an existing one, get rid of the connected users.
       if (username) {
         connectedUsers.delete(username);
       }
@@ -73,11 +99,11 @@ wss.on('connection', (conn, req) => {
       conn.send(JSON.stringify(
         { type: 'nick', nick: username }
       ));
-    
+
     }
-    // Sending a chat message.
-    else if (msg.type === 'chat') {
-      let { body } = msg;
+    // Joining a room
+    else if (msg.type === 'room') {
+      let { room } = msg;
 
       if (!username) {
         conn.send(JSON.stringify(
@@ -86,6 +112,61 @@ wss.on('connection', (conn, req) => {
         return;
       }
 
+      if (!room) {
+        conn.send(JSON.stringify(
+          { type: 'error', error: 'room_bounds' }
+        ));
+        return;
+      }
+
+      room = room.trim();
+
+      if (room.length < 0 || room.length > 16) {
+        conn.send(JSON.stringify(
+          { type: 'error', error: 'room_bounds' }
+        ));
+        return;
+      }
+
+      // Leave the current room
+      if (currentRoom) {
+        roomUsers[currentRoom].delete(username);
+        arrayDel(roomCons[currentRoom], conn);
+      }
+
+      console.log(`${username} switched from room ${currentRoom} to ${room}.`);
+
+      currentRoom = room;
+
+      // Join the room :-)
+      if (!(room in roomUsers)) {
+        roomUsers[room] = new Set();
+        roomCons[room] = [];
+      }
+
+      roomUsers[room].add(username);
+      roomCons[room].push(conn);
+    }
+    // Sending a chat message.
+    else if (msg.type === 'chat') {
+      let { body } = msg;
+
+      // Not yet registered
+      if (!username) {
+        conn.send(JSON.stringify(
+          { type: 'error', error: 'not_registered' }
+        ));
+        return;
+      }
+
+      if (!currentRoom) {
+        conn.send(JSON.stringify(
+          { type: 'error', error: 'not_in_room' }
+        ));
+        return;
+      }
+
+      // Empty message
       if (!body) {
         conn.send(JSON.stringify(
           { type: 'error', error: 'no_message' }
@@ -95,6 +176,7 @@ wss.on('connection', (conn, req) => {
 
       body = body.trim();
 
+      // Message too long?
       if (body.length < 1 || body.length > 1024) {
         conn.send(JSON.stringify(
           { type: 'error', error: 'message_bounds' }
@@ -102,9 +184,10 @@ wss.on('connection', (conn, req) => {
         return;
       }
 
-      // Returns true if request is allowed
+      // Rate limiting - returns true if request is allowed
       if (rateLimiter.hit(addr)) {
-        broadcast({ type: 'chat', nick: username, body });
+        broadcastToRoom(currentRoom, { type: 'chat', nick: username, body });
+        // broadcast({ type: 'chat', nick: username, body });
       } else {
         conn.send(JSON.stringify(
           { type: 'error', error: 'rate_limited' }
@@ -116,9 +199,18 @@ wss.on('connection', (conn, req) => {
   conn.on('close', () => {
     if (username !== null) {
       connectedUsers.delete(username);
+
+      if (currentRoom) {
+        roomUsers[currentRoom].delete(username);
+        arrayDel(roomCons[currentRoom], conn);
+      }
+
       console.log(`${username} disconnected.`);
     }
   })
 });
 
-server.listen(8080);
+const PORT = 8080;
+
+server.listen(PORT);
+console.log(`Chat server listening on ${PORT}.`);
