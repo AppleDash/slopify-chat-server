76cf423f1204d8f09dedadb3cda2aca3d76055b9
AppleDash <pet@feather.horse>
2024-12-31 01:18:54 -0500

    refactor: split out message handlers to separate functions

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import { WebSocketServer, WebSocket } from 'ws';
 import { RateLimiter } from './rate_limit.js';
 
 interface ConnectionState {
+  addr: string,
   conn: WebSocket,
   username?: string,
   room?: string
@@ -28,7 +29,6 @@ function getMaskedRemoteAddress(addr: string | string[] | undefined) {
     addr = addr[0];
   }
 
-  console.log('Addr', addr);
   // v6 address, stick a /64 mask on
   if (ip.isV6Format(addr)) {
     if (ip.isPrivate(addr)) {
@@ -60,6 +60,135 @@ function arrayDel(ary: unknown[], itm: unknown) {
   }
 }
 
+/** Called when we receive a message of type: nick from the client */
+function handleNick(state: ConnectionState, { nick }: { nick: string }) {
+  const { conn } = state;
+
+  nick = nick.trim();
+
+  // Nick is too short or too long.
+  if (nick.length < 1 || nick.length > 16) {
+    conn.send(JSON.stringify(
+      { type: 'error', error: 'nick_bounds' }
+    ));
+    return;
+  }
+
+  // Nick already taken
+  if (connectedUsers.has(nick)) {
+    conn.send(JSON.stringify(
+      { type: 'error', error: 'nick_taken' }
+    ));
+    return;
+  }
+
+  // If they're changing their name from an existing one, get rid of the connected users.
+  if (state.username) {
+    connectedUsers.delete(state.username);
+  }
+
+  connectedUsers.add(nick);
+
+  console.log(`User ${state.username} changed name to ${nick}`);
+  broadcast({ type: 'nick', prev_nick: state.username, nick: nick });
+
+  state.username = nick;
+  conn.send(JSON.stringify(
+    { type: 'nick', nick: state.username }
+  ));
+}
+
+function handleRoom(state: ConnectionState, { room }: { room: string }) {
+  const { conn, username } = state;
+
+  if (!username) {
+    conn.send(JSON.stringify(
+      { type: 'error', error: 'not_registered' }
+    ));
+    return;
+  }
+
+  if (!room) {
+    conn.send(JSON.stringify(
+      { type: 'error', error: 'room_bounds' }
+    ));
+    return;
+  }
+
+  room = room.trim();
+
+  if (room.length < 0 || room.length > 16) {
+    conn.send(JSON.stringify(
+      { type: 'error', error: 'room_bounds' }
+    ));
+    return;
+  }
+
+  // Leave the current room
+  if (state.room) {
+    roomUsers[state.room].delete(username);
+    arrayDel(roomCons[state.room], conn);
+  }
+
+  console.log(`${username} switched from room ${state.room} to ${room}.`);
+
+  state.room = room;
+
+  // Join the room :-)
+  if (!(room in roomUsers)) {
+    roomUsers[room] = new Set();
+    roomCons[room] = [];
+  }
+
+  roomUsers[room].add(username);
+  roomCons[room].push(conn);
+}
+
+function handleChat(state: ConnectionState, { body }: { body: string }) {
+  const { addr, conn, room, username } = state;
+   // Not yet registered
+   if (!username) {
+    conn.send(JSON.stringify(
+      { type: 'error', error: 'not_registered' }
+    ));
+    return;
+  }
+
+  if (!room) {
+    conn.send(JSON.stringify(
+      { type: 'error', error: 'not_in_room' }
+    ));
+    return;
+  }
+
+  // Empty message
+  if (!body) {
+    conn.send(JSON.stringify(
+      { type: 'error', error: 'no_message' }
+    ));
+    return;
+  }
+
+  body = body.trim();
+
+  // Message too long?
+  if (body.length < 1 || body.length > 1024) {
+    conn.send(JSON.stringify(
+      { type: 'error', error: 'message_bounds' }
+    ));
+    return;
+  }
+
+  // Rate limiting - returns true if request is allowed
+  if (rateLimiter.hit(addr)) {
+    broadcastToRoom(room, { type: 'chat', nick: username, body });
+  } else {
+    conn.send(JSON.stringify(
+      { type: 'error', error: 'rate_limited' }
+    ));
+  }
+}
+
 wss.on('connection', (conn, req) => {
   let addr;
 
@@ -70,157 +199,44 @@ wss.on('connection', (conn, req) => {
     addr = getMaskedRemoteAddress(req.socket.remoteAddress);
   }
 
+  let state : ConnectionState = { addr, conn };
+
   console.log('Accepting connection from', addr);
 
-  let state : ConnectionState = { conn };
-
   conn.on('message', (buffer) => {
-    const { username } = state;
     const msg = JSON.parse(buffer.toString());
-    const currentRoom = state.room;
 
     console.log('Message', msg);
+
     // They want to change their nick.
     if (msg.type === 'nick') {
-      let { nick } = msg;
-
-      nick = nick.trim();
-
-      // Nick is too short or too long.
-      if (nick.length < 1 || nick.length > 16) {
-        conn.send(JSON.stringify(
-          { type: 'error', error: 'nick_bounds' }
-        ));
-        return;
+      if (typeof(msg.nick) === 'string') {
+        handleNick(state, msg);
       }
-
-      // Nick already taken
-      if (connectedUsers.has(nick)) {
-        conn.send(JSON.stringify(
-          { type: 'error', error: 'nick_taken' }
-        ));
-        return;
-      }
-
-      // If they're changing their name from an existing one, get rid of the connected users.
-      if (state.username) {
-        connectedUsers.delete(state.username);
-      }
-
-      connectedUsers.add(nick);
-
-      console.log(`User ${state.username} changed name to ${nick}`);
-      broadcast({ type: 'nick', prev_nick: state.username, nick: nick });
-      state.username = nick;
-      conn.send(JSON.stringify(
-        { type: 'nick', nick: state.username }
-      ));
-
     }
     // Joining a room
     else if (msg.type === 'room') {
-      let { room } = msg;
-
-      if (!username) {
-        conn.send(JSON.stringify(
-          { type: 'error', error: 'not_registered' }
-        ));
-        return;
+      if (typeof(msg.room) === 'string') {
+        handleRoom(state, msg);
       }
-
-      if (!room) {
-        conn.send(JSON.stringify(
-          { type: 'error', error: 'room_bounds' }
-        ));
-        return;
-      }
-
-      room = room.trim();
-
-      if (room.length < 0 || room.length > 16) {
-        conn.send(JSON.stringify(
-          { type: 'error', error: 'room_bounds' }
-        ));
-        return;
-      }
-
-      // Leave the current room
-      if (state.room) {
-        roomUsers[state.room].delete(username);
-        arrayDel(roomCons[state.room], conn);
-      }
-
-      console.log(`${username} switched from room ${currentRoom} to ${room}.`);
-
-      state.room = room;
-
-      // Join the room :-)
-      if (!(room in roomUsers)) {
-        roomUsers[room] = new Set();
-        roomCons[room] = [];
-      }
-
-      roomUsers[room].add(username);
-      roomCons[room].push(conn);
     }
     // Sending a chat message.
     else if (msg.type === 'chat') {
-      let { body } = msg;
-
-      // Not yet registered
-      if (!username) {
-        conn.send(JSON.stringify(
-          { type: 'error', error: 'not_registered' }
-        ));
-        return;
-      }
-
-      if (!currentRoom) {
-        conn.send(JSON.stringify(
-          { type: 'error', error: 'not_in_room' }
-        ));
-        return;
-      }
-
-      // Empty message
-      if (!body) {
-        conn.send(JSON.stringify(
-          { type: 'error', error: 'no_message' }
-        ));
-        return;
-      }
-
-      body = body.trim();
-
-      // Message too long?
-      if (body.length < 1 || body.length > 1024) {
-        conn.send(JSON.stringify(
-          { type: 'error', error: 'message_bounds' }
-        ));
-        return;
-      }
-
-      // Rate limiting - returns true if request is allowed
-      if (rateLimiter.hit(addr)) {
-        broadcastToRoom(currentRoom, { type: 'chat', nick: username, body });
-        // broadcast({ type: 'chat', nick: username, body });
-      } else {
-        conn.send(JSON.stringify(
-          { type: 'error', error: 'rate_limited' }
-        ));
+      if (typeof(msg.body) === 'string') {
+        handleChat(state, msg);
       }
     }
   });
 
   conn.on('close', () => {
-    const { username } = state;
-    const currentRoom = state.room;
+    const { username, room } = state;
+
     if (username) {
       connectedUsers.delete(username);
 
-      if (currentRoom) {
-        roomUsers[currentRoom].delete(username);
-        arrayDel(roomCons[currentRoom], conn);
+      if (room) {
+        roomUsers[room].delete(username);
+        arrayDel(roomCons[room], conn);
       }
 
       console.log(`${username} disconnected.`);
